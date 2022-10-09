7cacdb760ad29303d81272149c5c35468c45d454
henryposa <luisgasthof@hotmail.com>
2022-10-09 14:11:47 -0500

    seeding a la base de datos

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { categorias } from "./data/categorias";
+import { productos } from "./data/productos";
+// Para hacer una operación en la base de datos
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+const main = async (): Promise<void> => {
+    try {
+        // Insertamos los datos. dentro del modelo .categoria
+        // va a agregar todas las categorias que están en data
+        await prisma.categoria.createMany({
+            data: categorias
+        })
+        await prisma.producto.createMany({
+            data: productos
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+main()
